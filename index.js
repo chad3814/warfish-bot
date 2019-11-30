6041ca2c0123b0a2651fb36f57d9f866543d9c38
Chad Walker <chad@chad-cat-lore-eddie.com>
2019-11-29 21:08:46 -0600

    base for slack commands, added `testSlack` graphql query

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const graphqlHTTP = require('express-graphql');
 
 const {schema, root} = require('./lib/graphql');
 
-const app = express();
+const SlackCommands = require('./lib/slack-commands');
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true,
-}));
+const main = async function () {
+    await SlackCommands.init();
 
-app.listen(4000);
+    const app = express();
 
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+    app.use('/graphql', graphqlHTTP({
+        schema,
+        rootValue: root,
+        graphiql: true,
+    }));
+
+    app.use('/slack', SlackCommands.getMiddleWare());
+
+    app.listen(4000);
+    console.log('Running a GraphQL API server at localhost:4000/graphql');  
+};
+
+main();
